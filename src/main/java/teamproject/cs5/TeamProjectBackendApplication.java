94f4eead544718968d8882f20c16daf57b2bd340
maciekd12 <maciek.pilot2@gmail.com>
2022-04-27 15:10:59 +0200

    Initial commit

---
+++ src/main/java/teamproject/cs5/TeamProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package teamproject.cs5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/teamproject/cs5/TeamProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package teamproject.cs5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
